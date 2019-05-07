528bc4ac7a73aa04ae7aa7966658f9f49a4f37f5
Gomer <gomerbabenko@gmail.com>
2019-05-08 00:27:04 +0300

    Final

--- hw05/js/main.js
+++ hw05/js/main.js
@@ -3,22 +3,23 @@
 
 
   $(document).ready(function () {
-    
-    (function ($) {
 
-      
+      $("a[rel='m_PageScroll2id']").mPageScroll2id({
+        offset:50
+      });
+
       // Mobile menu
       $('.mobile-menu-button').on('click', function() {
-        $('.menu').toggleClass('collapse');
+        $('.mainNav__items').toggleClass('collapse');
       });
 
       // Scripts which runs at window resize
       $(window).resize(function () {
-        if ( $(window).width() > 640 ) {
-          $('.menu').removeClass('collapse');
+        if ( $(window).width() > 720 ) {
+          $('.mainNav__items').removeClass('collapse');
           //$('.searchform').removeClass('collapse');
         } else {
-          $('.menu').addClass('collapse');
+          $('.mainNav__items').addClass('collapse');
           //$('.searchform').addClass('collapse');
 
         }
@@ -53,11 +54,12 @@
         nextArrow: '<i class="fas fa-chevron-right fa-2x slick-next"></i>',
       });
 
-    }(jQuery));
+
   });
 
 
   $(window).load(function () {
+    
     
   });
 
