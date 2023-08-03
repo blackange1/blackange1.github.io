ab377d86110c8c04722296998b64dd63711d1b46
Volodymyr Babenko <v.babenko@netframe.org>
2023-08-03 16:57:04 +0300

    edit js

--- nf/create_lead/send_form.js
+++ nf/create_lead/send_form.js
@@ -2,33 +2,39 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const print = console.log
     const $form = document.getElementById('form_create_lead')
     const $submit = document.getElementById('submit_create_lead')
-    // const urlAPI = 'http://localhost:8069/api/create_lead?'
-    const urlAPI = 'https://160c-185-19-6-88.ngrok-free.app' + '/api/create_lead?'
+    const urlAPI = 'https://160c-185-19-6-88.ngrok-free.app' + '/api/create_lead'
     const nameInputs = ['name', 'email', 'phone']
 
     $form.onclick = function (event) {
         event.preventDefault()
     }
     $submit.onclick = function () {
-    // document.getElementById('superbatton').onclick = function () {
+        // document.getElementById('superbatton').onclick = function () {
         // let inputs = []
-        print('superbatton', superbatton)
-        let paramForUrl = []
+        let bodyValue = {}
         for (const name of nameInputs) {
             const $input = $form.querySelector(`input[name="${name}"]`)
-            paramForUrl.push(`${name}=${$input.value}`)
+            // paramForUrl.push(`${name}=${$input.value}`)
+            bodyValue[name] = $input.value
             // inputs.push($input)
         }
+        print(bodyValue)
         // print(inputs)
-        print(paramForUrl.join('&'))
-        let parametersUrl = ''
-        const url = `${urlAPI}${paramForUrl.join('&')}`
-        fetch(url, {
-            method: 'GET',
+        // print(paramForUrl.join('&'))
+        // let parametersUrl = ''
+        // const url = `${urlAPI}${paramForUrl.join('&')}`
+        fetch(urlAPI, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 // 'Access-Control-Allow-Origin': 'http://localhost:63342'
             },
+            body: JSON.stringify({
+                'name': 'qwerty',
+                'email': 'qwerty',
+                'phone': 'qwerty',
+            })
+
         })
             .then(response => response.json())
             .then(response => {
