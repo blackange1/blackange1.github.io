e6c358defd262637971ad4b2ef9eb005d42b4d5b
Volodymyr Babenko <v.babenko@netframe.org>
2023-08-03 11:58:58 +0300

    add urlAPI in js

--- nf/create_lead/send_form.js
+++ nf/create_lead/send_form.js
@@ -2,21 +2,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const print = console.log
     const $form = document.getElementById('form_create_lead')
     const $submit = document.getElementById('submit_create_lead')
-    const urlAPI = 'http://localhost:8069/uk/api/create_lead?'
+    // const urlAPI = 'http://localhost:8069/uk/api/create_lead?'
+    const urlAPI = 'https://3577-185-19-6-88.ngrok-free.app'
     const nameInputs = ['name', 'email', 'phone']
 
     $form.onclick = function (event) {
         event.preventDefault()
     }
     $submit.onclick = function () {
-        let inputs = []
+        // let inputs = []
         let paramForUrl = []
         for (const name of nameInputs) {
             const $input = $form.querySelector(`input[name="${name}"]`)
             paramForUrl.push(`${name}=${$input.value}`)
-            inputs.push($input)
+            // inputs.push($input)
         }
-        print(inputs)
+        // print(inputs)
         print(paramForUrl.join('&'))
         let parametersUrl = ''
         const url = `${urlAPI}${paramForUrl.join('&')}`
