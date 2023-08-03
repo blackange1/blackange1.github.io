1b2988f72ef0a0a73449acfd33dab6e1b912e1f3
Volodymyr Babenko <v.babenko@netframe.org>
2023-08-03 15:57:32 +0300

    edit js

--- nf/create_lead/send_form.js
+++ nf/create_lead/send_form.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const $form = document.getElementById('form_create_lead')
     const $submit = document.getElementById('submit_create_lead')
     // const urlAPI = 'http://localhost:8069/api/create_lead?'
-    const urlAPI = 'https://1624-185-19-6-88.ngrok-free.app' + '/api/create_lead?'
+    const urlAPI = 'https://160c-185-19-6-88.ngrok-free.app' + '/api/create_lead?'
     const nameInputs = ['name', 'email', 'phone']
 
     $form.onclick = function (event) {
