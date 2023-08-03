186aa76e964c49e8be95eeeab7d0046091d74145
Volodymyr Babenko <v.babenko@netframe.org>
2023-08-03 15:55:33 +0300

    edit js

--- nf/create_lead/send_form.js
+++ nf/create_lead/send_form.js
@@ -3,14 +3,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const $form = document.getElementById('form_create_lead')
     const $submit = document.getElementById('submit_create_lead')
     // const urlAPI = 'http://localhost:8069/api/create_lead?'
-    const urlAPI = 'https://3577-185-19-6-88.ngrok-free.app' + '/api/create_lead?'
+    const urlAPI = 'https://1624-185-19-6-88.ngrok-free.app' + '/api/create_lead?'
     const nameInputs = ['name', 'email', 'phone']
 
     $form.onclick = function (event) {
         event.preventDefault()
     }
     $submit.onclick = function () {
+    // document.getElementById('superbatton').onclick = function () {
         // let inputs = []
+        print('superbatton', superbatton)
         let paramForUrl = []
         for (const name of nameInputs) {
             const $input = $form.querySelector(`input[name="${name}"]`)
@@ -24,7 +26,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
         fetch(url, {
             method: 'GET',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                // 'Access-Control-Allow-Origin': 'http://localhost:63342'
             },
         })
             .then(response => response.json())
